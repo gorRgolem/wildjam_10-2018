cb3094d26e952dde8c4fdcfea8b8cebd2937dbf6
Estelle Rostan <estellerostan@pm.me>
2018-10-19 22:10:36 +0200

    Make the enemy follow the player
    It won't stop no matter what so you better run!

--- Enemy.gd
+++ Enemy.gd
@@ -1,15 +1,16 @@
 extends RigidBody
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var speed = 12
+onready var player = get_node("../Chicken")
 
 func _ready():
-	set_contact_monitor( true )
-	set_max_contacts_reported( 5 )
-#	connect("body_enter",self,"body_enter")
+	pass
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+func _physics_process(delta):
+	var player_pos = player.get_translation()
+	translate((player_pos - get_translation()).normalized() * speed * delta)
